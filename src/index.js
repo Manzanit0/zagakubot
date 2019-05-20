a0a7dec09fc5f32bf8ec4766b0d10bd41d1cee1c
Manzanit0 <unico.javier@gmail.com>
2019-05-20 14:30:06 +0100

    Adds an /oauth endpoint for installation.
    The /oauth endpoint is necessary in order to be able to install
    our application in different teams, for example via de 'Add' button
    which Slack provides in 'https://api.slack.com/docs/slack-button'.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const request = require('request');
+
+// Slack API credentials
+const clientId = '<client Id here>';
+const clientSecret = '<secret here>';
 
 const app = express();
 
 // The endpoint for the webhook
 app.post('/command', (req, res) => res.send('Your ngrok tunnel is up and running!'));
+
+app.get('/oauth', (req, res) => {
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+    } else {
+        request({
+            url: 'https://slack.com/api/oauth.access',
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
+            method: 'GET',
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+            }
+        })
+    }
+});
 
 const PORT = 4390;
 app.listen(PORT, () => console.log("Example app listening on port " + PORT));
